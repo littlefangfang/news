60ae3dc44d55a147911ecd92e9b2dd0e6d9bb6ff
littlefangfang <fy@fys-macbook-pro.local>
2016-03-29 22:29:43 +0800

    start

---
+++ News(oc)/News(oc)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  News(oc)
+//
+//  Created by fy on 16/3/29.
+//  Copyright © 2016年 founder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ News(oc)/News(oc)/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  News(oc)
+//
+//  Created by fy on 16/3/29.
+//  Copyright © 2016年 founder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
