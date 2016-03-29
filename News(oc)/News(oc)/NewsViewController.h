13941a71c1e54a5f1a7fc6554b999e4178f39e10
littlefangfang <fy@fys-macbook-pro.local>
2016-03-29 23:22:41 +0800

    2

---
+++ News(oc)/News(oc)/NewsViewController.h
@@ -0,0 +1,18 @@
+//
+//  NewsViewController.h
+//  News(oc)
+//
+//  Created by fy on 16/3/29.
+//  Copyright © 2016年 founder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *newsImageView;
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+
+@property (strong, nonatomic) IBOutlet UILabel *contentLabel;
+
+
+@end
---
+++ News(oc)/News(oc)/NewsViewController.m
@@ -0,0 +1,54 @@
+//
+//  NewsViewController.m
+//  News(oc)
+//
+//  Created by fy on 16/3/29.
+//  Copyright © 2016年 founder. All rights reserved.
+//
+
+#import "NewsViewController.h"
+#import "NewsViewController.h"
+
+@interface NewsViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@end
+
+@implementation NewsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableView Data Source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 0;
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
